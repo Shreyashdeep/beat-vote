50311e47b1f2fb7ced47ed594abf5d3002513273
Shreyashdeep <shreyashdeep20@gmail.com>
2025-01-02 22:19:59 +0530

    refactor: simplify parameter handling in Creator component

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,19 +1,30 @@
+// import StreamView from "@/app/components/StreamView"
+
+// export default async function Creator({
+//     params:{
+//         creatorId
+//     }
+// }:{
+//     params:{
+//         creatorId:string;
+//     }
+// }){
+//     return <div>
+//         <StreamView creatorId={creatorId} playVideo={false}/>
+//     </div>
+// }
+
 import StreamView from "@/app/components/StreamView"
 
 export default async function Creator({
-    params:{
-        creatorId
-    }
+    params
 }:{
-    params:{
-        creatorId:string;
-    }
+    params: Promise<{ creatorId: string }>
 }){
+    const { creatorId } = await params;
     return <div>
         <StreamView creatorId={creatorId} playVideo={false}/>
     </div>
 }
-
-
 
 
